5b7f65c4232ddd5493bf3330180482f95f6dc5aa
lizhen <lizhen@outlook.jp>
2026-07-11 16:53:39 +0800

    find-first-and-last-position-of-element-in-sorted-array

---
+++ src/find-first-and-last-position-of-element-in-sorted-array.cc
@@ -0,0 +1,116 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <memory>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+// https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+namespace FindFirstAndLastPositionOfElementInSortedArray {
+class Solution {
+public:
+  vector<int> searchRange(vector<int> &nums, int target) {
+    if (nums.empty()) {
+      return {-1, -1};
+    }
+    if (nums.size() == 1) {
+      return nums[0] == target ? vector<int>{0, 0} : vector<int>{-1, -1};
+    }
+    if (nums[0] > target) {
+      return {-1, -1};
+    }
+    if (nums.size() < 4) {
+      auto begin = -1;
+      auto end = -1;
+      for (auto i = nums.begin(); i < nums.end(); ++i) {
+        if (*i == target) {
+          if (begin == -1) {
+            begin = distance(nums.begin(), i);
+          }
+          end = distance(nums.begin(), i);
+        }
+      }
+      return {begin, end};
+    }
+    auto begin = nums.begin();
+    auto end = nums.end();
+    while (begin < end) {
+      auto middle = begin + (end - begin) / 2;
+      if (target < *middle) {
+        end = middle;
+        continue;
+      }
+      if (target > *middle) {
+        begin = middle + 1;
+        continue;
+      }
+      auto left = middle;
+      auto right = middle;
+      while (left > nums.begin() && *(left - 1) == target) {
+        --left;
+      }
+      while (right < nums.end() - 1 && *(right + 1) == target) {
+        ++right;
+      }
+      return {int(distance(nums.begin(), left)),
+              int(distance(nums.begin(), right))};
+    }
+    return {-1, -1};
+  }
+};
+} // namespace FindFirstAndLastPositionOfElementInSortedArray
+
+struct FindFirstAndLastPositionOfElementInSortedArrayCase {
+  vector<int> nums;
+  int target;
+};
+
+namespace Catch {
+template <>
+struct StringMaker<FindFirstAndLastPositionOfElementInSortedArrayCase> {
+  static string
+  convert(FindFirstAndLastPositionOfElementInSortedArrayCase const &value) {
+    stringstream buffer;
+    buffer << "<[";
+    if (!value.nums.empty()) {
+      buffer << value.nums[0];
+      for (size_t i = 1; i < value.nums.size(); ++i) {
+        buffer << ", " << value.nums[i];
+      }
+    }
+    buffer << "], " << value.target << ">";
+    return buffer.str();
+  }
+};
+} // namespace Catch
+
+TEST_CASE("Case 1",
+          "[find-first-and-last-position-of-element-in-sorted-array]") {
+  using Solution = FindFirstAndLastPositionOfElementInSortedArray::Solution;
+  auto solution = make_unique<Solution>();
+  SECTION("find-first-and-last-"
+          "positiFindFirstAndLastPositionOfElementInSortedArrayon-of-element-"
+          "in-sorted-array official cases") {
+    FindFirstAndLastPositionOfElementInSortedArrayCase input;
+    vector<int> expect;
+    tie(input, expect) = GENERATE(
+        table<FindFirstAndLastPositionOfElementInSortedArrayCase, vector<int>>(
+            {make_tuple(
+                 FindFirstAndLastPositionOfElementInSortedArrayCase{
+                     vector<int>{5, 7, 7, 8, 8, 10}, 8},
+                 vector<int>{3, 4}),
+             make_tuple(
+                 FindFirstAndLastPositionOfElementInSortedArrayCase{
+                     vector<int>{5, 7, 7, 8, 8, 10}, 6},
+                 vector<int>{-1, -1}),
+             make_tuple(
+                 FindFirstAndLastPositionOfElementInSortedArrayCase{
+                     vector<int>{}, 0},
+                 vector<int>{-1, -1})}));
+    auto got = solution->searchRange(input.nums, input.target);
+    CAPTURE(input);
+    REQUIRE(got == expect);
+  }
+}
