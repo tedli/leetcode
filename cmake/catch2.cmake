fe8852aa410099600259545d47ca4b20e2b86128
lizhen <lizhen@outlook.jp>
2026-08-22 15:27:09 +0800

    valid-sudoku

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -3,8 +3,8 @@ include_guard()
 include(cmake/utils.cmake)
 
 FetchContent_DeclareGitHubTarWithMirror(catch2
-  catchorg/Catch2 v3.15.0
-  SHA256=9650c55e497759cc39b977e45524bc8acb15256061c112080916ab6cb0b1ea66
+  catchorg/Catch2 v3.15.3
+  SHA256=b0299ae552918220a7a6e21e7de5b714777f4e8c883fb70c4bb23fe01df8c6e3
 )
 
 FetchContent_MakeAvailableWithArgs(catch2
---
+++ src/202608/valid-sudoku.cc
@@ -0,0 +1,93 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <memory>
+#include <tuple>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// https://leetcode.cn/problems/valid-sudoku/description/
+
+namespace ValidSudoku {
+class Solution {
+public:
+  bool isValidSudoku(vector<vector<char>> &board) {
+    auto columns = array<unordered_set<char>, 9>();
+    for (auto i = board.begin(); i != board.end(); ++i) {
+      auto row = unordered_set<char>(9);
+      for (auto j = i->begin(); j != i->end(); ++j) {
+        if (*j == '.') {
+          continue;
+        }
+        if (row.find(*j) != row.end()) {
+          return false;
+        }
+        row.insert(*j);
+        auto &column = columns[distance(i->begin(), j)];
+        if (column.find(*j) != column.end()) {
+          return false;
+        }
+        column.insert(*j);
+      }
+    }
+    for (int i = 0; i < 9; ++i) {
+      auto box = unordered_set<char>(9);
+      for (int j = 0; j < 9; ++j) {
+        auto row = i / 3 * 3 + j / 3;
+        auto column = i % 3 * 3 + j % 3;
+        if (board[row][column] == '.') {
+          continue;
+        }
+        if (box.find(board[row][column]) != box.end()) {
+          return false;
+        }
+        box.insert(board[row][column]);
+      }
+    }
+    return true;
+  }
+};
+} // namespace ValidSudoku
+
+TEST_CASE("Case 1", "[valid-sudoku]") {
+  auto solution = make_unique<ValidSudoku::Solution>();
+  SECTION("valid-sudoku official cases") {
+    vector<vector<char>> input;
+    bool expect;
+    tie(input, expect) = GENERATE(table<vector<vector<char>>, bool>(
+        {{{{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+           {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+           {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+           {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+           {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+           {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+           {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+           {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+           {'.', '.', '.', '.', '8', '.', '.', '7', '9'}},
+          true},
+         {{{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+           {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+           {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+           {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+           {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+           {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+           {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+           {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+           {'.', '.', '.', '.', '8', '.', '.', '7', '9'}},
+          false},
+         {{{'.', '.', '4', '.', '.', '.', '6', '3', '.'},
+           {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+           {'5', '.', '.', '.', '.', '.', '.', '9', '.'},
+           {'.', '.', '.', '5', '6', '.', '.', '.', '.'},
+           {'4', '.', '3', '.', '.', '.', '.', '.', '1'},
+           {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
+           {'.', '.', '.', '5', '.', '.', '.', '.', '.'},
+           {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+           {'.', '.', '.', '.', '.', '.', '.', '.', '.'}},
+          false}}));
+    auto got = solution->isValidSudoku(input);
+    CAPTURE(input);
+    REQUIRE(got == expect);
+  }
+}
